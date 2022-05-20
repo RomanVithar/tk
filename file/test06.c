a9d19c9e6eb680c4db19013b09492410449551d9
Roman <58694421+RomanVithar@users.noreply.github.com>
2022-05-20 14:46:04 +0300

    Update test06.c

--- file/test06.c
+++ file/test06.c
@@ -1,14 +1,14 @@
 int main() {
-int count = 10;
-int count2 = 10;
-while(count) {
-while(count2 != 5){
-count = count - 2
-if(count == 6) {
-break;
-}
-}
-count--;
-}
-return 0;
+  int count = 10;
+  int count2 = 10;
+  while(count) {
+    while(count2 != 5){
+      count = count - 2;
+      if(count == 6) {
+        break;
+      }
+    }
+  count--;
+  }
+  return 0;
 }
