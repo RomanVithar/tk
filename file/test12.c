8a234a5b072719c1406e47663d18309d1f9ff42a
Roman Anokhin <58694421+vithar1@users.noreply.github.com>
2022-06-29 16:55:52 +0300

    Update test12.c

--- file/test12.c
+++ file/test12.c
@@ -1,28 +1,28 @@
 
 int main() {
-int [5]arr1 = {1,2,3,4,5};
-int [5]arr2 = {5,4,3,2,1};
+int [5]arr1;
+int [5]arr2;
 int [10]arr3;
 int count = 0;
 float x = 0.0;
 bool flag = false;
-for(int i=0;i<10;i++) {
-count++;
+for(int i=0;i<10;i = i+1) {
+count = count - 1;
 if(count < 5) {
-[i]arr3=[i]arr;
+arr3[i]=arr[i];
 if(count*25 == 50) {
 flag = true;
 }
 if(flag) {
 while(x < 43) {
-x++;
+x = x+1;
 if (x > 5) {
 x = x * 1.1 + 7;
 }
 }
 }
 } else {
-[i]arr3=[i-5]arr;
+arr3[i]=arr[i-5];
 }
 }
 return 0;
